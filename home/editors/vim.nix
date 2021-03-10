6ad42637fcf8b7a97926002b15184eba53383fd2
Karl Meakin <karlwfmeakin@gmail.com>
2021-03-10 06:42:07 +0000

    vim config

--- home/editors/vim.nix
+++ home/editors/vim.nix
@@ -1,1 +1,9 @@
-{ ... }: {}
+{ config, lib, pkgs, ... }: {
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+  home.sessionVariables = { EDITOR = "${pkgs.neovim}/bin/nvim"; };
+}
