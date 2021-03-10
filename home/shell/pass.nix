15b5654ff6016f947f8eae53de0b1f80ac31f863
Karl Meakin <karlwfmeakin@gmail.com>
2021-03-10 14:28:35 +0000

    pass-git-helper

--- home/shell/pass.nix
+++ home/shell/pass.nix
@@ -3,10 +3,23 @@
 
   programs.password-store = {
     enable = true;
-
   };
 
   home.sessionVariables = {
-    PASSWORD_STORE_DIR = "${config.xdg.configHome}";
+    PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+    GNUPGHOME = "${config.xdg.dataHome}/gnupg";
+  };
+
+  programs.git.extraConfig = {
+    credential = {
+      helper = "${pkgs.gitAndTools.pass-git-helper}/bin/pass-git-helper";
+    };
+  };
+
+  xdg.configFile."pass-git-helper/git-pass-mapping.ini".text = lib.generators.toINI {} {
+    DEFAULT.skip_username = 7;
+    "github.com".target = "github.com";
+    "gitlab.com".target = "gitlab.com";
+    "gitlab.cs.man.ac.uk".target = "manchester.ac.uk";
   };
 }
