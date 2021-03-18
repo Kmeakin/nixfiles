762bfe0bd60b554afc3ac5bc77a6de858a93748c
Karl Meakin <karlwfmeakin@gmail.com>
2021-03-18 16:35:03 +0000

    add some more vscode extensions

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -4,6 +4,21 @@
     extensions = with pkgs.vscode-extensions; [
       bbenoist.Nix
       ms-vsliveshare.vsliveshare
-    ];
+    ] ++ (
+      pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "nix-env-selector";
+          publisher = "arrterian";
+          version = "1.0.6";
+          sha256 = "tOGsy4fm5KToKj3ci3LjAn5gwcI4WLt8MI7XCLMFZqY=";
+        }
+        {
+          name = "discord-vscode";
+          publisher = "icrawl";
+          version = "5.4.0";
+          sha256 = "yQwOlUK2Xifc0y1HIvyKss73OCxpZBKnTqMrNgT5YFo=";
+        }
+      ]
+    );
   };
 }
