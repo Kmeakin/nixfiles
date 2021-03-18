8a5ab2285e16bc7d6305853a4a27be16c7adc2a1
Karl Meakin <karlwfmeakin@gmail.com>
2021-03-18 16:34:49 +0000

    setup vscode for rust

--- home/dev/rust.nix
+++ home/dev/rust.nix
@@ -5,10 +5,10 @@
       combine (
         with latest; [
           cargo
-          clippy-preview
+          clippy
           rust-std
           rustc
-          rustfmt-preview
+          rustfmt
           rust-src
         ]
       )
@@ -24,7 +24,27 @@
   home.sessionVariables = {
     CARGO_HOME = "${config.xdg.dataHome}/cargo";
     RUSTC_WRAPPER = "sccache";
+
+    # needed for `cargo expand`
+    RUSTFMT = "$(which rustfmt)";
   };
 
-  programs.vscode.extensions = with pkgs.vscode-extensions; [];
+  programs.vscode.extensions = with pkgs.vscode-extensions; [
+    matklad.rust-analyzer
+  ] ++ (
+    pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "better-toml";
+        publisher = "bungcip";
+        version = "0.3.2";
+        sha256 = "g+LfgjAnSuSj/nSmlPdB0t29kqTmegZB5B1cYzP8kCI=";
+      }
+      {
+        name = "insta";
+        publisher = "mitsuhiko";
+        version = "1.0.6";
+        sha256 = "sha256-Z/bU7h0GJyHBXNwk7uSsOPdRf3RzJcwWroWgXZnlAbA=";
+      }
+    ]
+  );
 }
