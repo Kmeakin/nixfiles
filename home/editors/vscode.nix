c5e5fb0a55e2c1f95ede7b9e02dbd84a33a68ff7
Karl Meakin <karlwfmeakin@gmail.com>
2021-03-29 00:23:08 +0100

    vscode extensions

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -2,10 +2,40 @@
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
-      bbenoist.Nix
       ms-vsliveshare.vsliveshare
+      vscodevim.vim
     ] ++ (
       pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vspacecode";
+          publisher = "vspacecode";
+          version = "0.9.1";
+          sha256 = "/qJKYXR0DznqwF7XuJsz+OghIBzdWjm6dAlaRX4wdRU=";
+        }
+        {
+          name = "whichkey";
+          publisher = "vspacecode";
+          version = "0.8.4";
+          sha256 = "cS8wFNJ5TTfj3eRxWLtYKyyGXoge1lv4SyvxjEAeHS4=";
+        }
+        {
+          publisher = "bodil";
+          name = "file-browser";
+          version = "0.2.10";
+          sha256 = "RW4vm0Hum9AeN4Rq7MSJOIHnALU0L1tBLKjaRLA2hL8=";
+        }
+        {
+          publisher = "kahole";
+          name = "magit";
+          version = "0.6.7";
+          sha256 = "rlQmvR2dlTzJj9lHYDSuLZXo40OMuu0sd/zY9epZXsY=";
+        }
+        {
+          name = "material-theme";
+          publisher = "zhuangtongfa";
+          version = "3.9.15";
+          sha256 = "q4pIZtCmQrTd7o5UQXBt/G31KDN3po53XxDnmWB7UVo=";
+        }
         {
           name = "nix-env-selector";
           publisher = "arrterian";
