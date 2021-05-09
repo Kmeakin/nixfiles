de422fa1f504daea0765cbdc4b6950955d5239ae
Karl Meakin <karlwfmeakin@gmail.com>
2021-05-09 07:07:08 +0100

    remove java lsp - not needed anymore

--- home/dev/java.nix
+++ home/dev/java.nix
@@ -11,15 +11,4 @@
       -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel
     '';
   };
-
-  programs.vscode.extensions = with pkgs.vscode-extensions; [] ++ (
-    pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "java";
-        publisher = "redhat";
-        version = "0.76.0";
-        sha256 = "XoyPhos3+fMllQIUI0M305pQWzT/4Ul2vABoEmdeaXU=";
-      }
-    ]
-  );
 }
