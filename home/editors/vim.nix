3333f67a8ba5a0b8d97a4653b563d26836d80e14
Karl Meakin <karlwfmeakin@gmail.com>
2021-05-09 07:05:38 +0100

    vim config

--- home/editors/vim.nix
+++ home/editors/vim.nix
@@ -1,9 +1,46 @@
 { config, lib, pkgs, ... }: {
+  home.sessionVariables = { EDITOR = "${pkgs.neovim}/bin/nvim"; };
+
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    extraConfig = ''
+      set nocompatible
+      " Syntax highlighting
+      filetype plugin on
+      syntax on
+
+      " Indentation
+      filetype indent on
+
+      " Line numbers
+      set number
+
+      " Set size of tabs to 4 spaces
+      set tabstop=4
+      set shiftwidth=4
+
+      " Don't expand tab to spaces
+      set expandtab!
+
+      " Highlight search matches
+      set hlsearch
+
+      " Use incremental searching
+      set incsearch
+
+      " Ignore case in searches, unless the search pattern contains uppercase
+      set ignorecase
+      set smartcase
+
+      " Copy indent from current line when starting a new line
+      set autoindent
+
+      " Auto-create directories for new files
+      au BufWritePre,FileWritePre * silent! call mkdir(expand('<afile>:p:h'), 'p')
+    '';
   };
-  home.sessionVariables = { EDITOR = "${pkgs.neovim}/bin/nvim"; };
 }
