a98cad29ddb6d36452b4e77283526d9c795c43d5
Karl Meakin <karlwfmeakin@gmail.com>
2021-05-27 21:11:34 +0100

    remove unused python extensions

--- home/dev/python.nix
+++ home/dev/python.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, ... }: {
   home.packages = with pkgs; [
     python3
-    black
-    python3Packages.pylama
-  ];
-
-  programs.vscode.extensions = with pkgs.vscode-extensions; [
-    ms-python.python
   ];
 }
