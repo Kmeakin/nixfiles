8ee1ebc9a85a94c2986f3e67eddeae05d784e926
Karl Meakin <karlwfmeakin@gmail.com>
2024-06-01 23:04:26 +0100

    Add pass-git-helper

--- home/shell/pass.nix
+++ home/shell/pass.nix
@@ -4,4 +4,16 @@ inputs @ { config, pkgs, ... }:
   home.sessionVariables = {
     PASSWORD_STORE_DIR = "${config.xdg.dataHome}/passwords";
   };
+
+  programs.git.extraConfig = {
+    credential = {
+      helper = "${pkgs.gitAndTools.pass-git-helper}/bin/pass-git-helper";
+    };
+  };
+
+  xdg.configFile."pass-git-helper/git-pass-mapping.ini".text = inputs.lib.generators.toINI { } {
+    DEFAULT.skip_username = inputs.lib.stringLength "login: ";
+    "github.com".target = "github-cli";
+    "gitlab.com".target = "gitlab.com";
+  };
 }
